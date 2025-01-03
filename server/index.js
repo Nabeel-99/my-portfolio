46959ab09de6b773d7914c05b3f588ba93c76de7
Farouk Nabeel <farouknabil810@gmail.com>
2025-01-03 23:33:59 +0100

    updated

--- server/index.js
+++ server/index.js
@@ -30,8 +30,9 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.json("hello");
+  return res.json("hello");
 });
+
 app.post("/send-email", async (req, res) => {
   try {
     const { name, email, message, budget } = req.body;
